6e5ce7232323a44fb0394fa7562fa86b3df0e11f
KRKimDongWoo <dhsodhao52@naver.com>
2019-04-25 14:01:30 +0900

    modified App.js and Notfound page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Route, Switch, BrowserRouter } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Provider } from "react-redux"
+import { createStore } from "redux"
+
+import Main from "./components/Main"
+import NotFound from "./components/NotFound"
+
+class App extends Component {
+	render() {
+		return (
+			<BrowserRouter>
+				<Switch>
+					<Route exact path="/" component={Main} />
+					<Route component={NotFound} />
+				</Switch>
+			</BrowserRouter>
+		)
+	}
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+	render() {
+		return (
+			<div>
+				<img src="../logos/top-logo-blue.png"/>
+			</div>
+		)
+	}
+}
+
+export default Main

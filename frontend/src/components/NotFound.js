ceadda0cad6f05a34391789bfe3173a5724d7349
KRKimDongWoo <dhsodhao52@naver.com>
2019-04-25 16:52:50 +0900

    adjust conflicts

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -5,11 +5,7 @@ const NotFound = () => {
 	return (
 		<div>
 			<h2> Not Found </h2>
-<<<<<<< HEAD
-			<p> The page you're looking for does not exist.</p>
-=======
 			<p> The page you're looking for does not exist. </p>
->>>>>>> b6ae231dba8c0a6cecf6b523845538895f39c367
 			<p>
 				Click
 				<Link to="/"> here</Link>

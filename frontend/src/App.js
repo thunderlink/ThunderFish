03b404d1c8230fc35b36972edda6446acca8b9d3
KRKimDongWoo <dhsodhao52@naver.com>
2019-04-27 16:46:09 +0900

    moved to main repo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Route, Switch, BrowserRouter } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Provider } from "react-redux"
+import { createStore } from "redux"
+
+import Main from "./components/Main"
+import NotFound from "./components/NotFound"
+import Toolbar from "./components/Toolbar"
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<Toolbar />
+				<main style={{marginTop: '70px'}}>
+					<BrowserRouter>
+						<Switch>
+							<Route exact path="/" component={Main} />
+							<Route component={NotFound} />
+						</Switch>
+					</BrowserRouter>
+				</main>
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App

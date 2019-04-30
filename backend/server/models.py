8a5d63eedbbbacd52d665792d4eda8481726df6f
Sungchan Yi <calofmijuck@snu.ac.kr>
2019-04-30 21:25:26 +0900

    Models almost finished!

--- backend/server/models.py
+++ backend/server/models.py
@@ -10,7 +10,7 @@ class User(models.Model):
     email = models.EmailField(max_length=30)
     name = models.CharField(max_length=50)
     gender = models.IntegerField()
-    # region
+    region = models.CharField(max_length=100, default="")  # may not be necessary, use API ??
     introduce = models.CharField(max_length=200)
     # meeting_hosted
     # meeting_participated
@@ -24,19 +24,19 @@ class User(models.Model):
 
 class Meeting(models.Model):
     name = models.CharField(max_length=50)
-    host = models.ForeignKey(User, on_delete=models.CASCADE)
+    host = models.ForeignKey(User, related_name="meeting_hosted", on_delete=models.CASCADE)
     date = models.DateTimeField('meeting date')
-    # participant - Accepted
-    # waiter - Pending accept
+    participant = models.ManyToManyField(User, related_name="meeting_participated")
+    waiter = models.ManyToManyField(User, related_name="meeting_waiting")
     # contributer - people who opened the meeting with the host
     max_participant = models.IntegerField()
     deadline = models.DateTimeField('meeting deadline')
-    # region
+    region = models.CharField(max_length=100, default="")
     photo = models.ImageField(upload_to=pic_folder, default=DEFAULT_IMAGE)
     content = models.CharField(max_length=500)
-    # tag
+    # tag : Array of chars
     status = models.IntegerField() # 1 as pending, 0 as complete ?
-    # comment
+    open_chat = models.URLField(max_length=100, default="open")
 
     def __str__(self):
         return self.name
@@ -48,8 +48,8 @@ class Comment(models.Model):
     date = models.DateTimeField('comment date', auto_now_add=True)
     comment_text = models.CharField(max_length=1000, default="Test Text")
     # parent_comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
-    parent_meeting = models.ForeignKey(Meeting, on_delete=models.CASCADE)
-    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    parent_meeting = models.ForeignKey(Meeting, related_name="comments", on_delete=models.CASCADE)
+    writer = models.ForeignKey(User, related_name="my_comments", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.comment_text
--- backend/server/serializers.py
+++ backend/server/serializers.py
@@ -4,12 +4,12 @@ from .models import User, Meeting, Comment
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'nickname', 'photo', 'email', 'name', 'gender', 'introduce')
+        fields = ('id', 'nickname', 'photo', 'email', 'name', 'gender', 'region', 'introduce', 'meeting_hosted', 'my_comments', 'meeting_participated', 'meeting_waiting')
 
 class MeetingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Meeting
-        fields = ('id', 'name', 'host', 'date', 'max_participant', 'deadline', 'photo', 'content', 'status')
+        fields = ('id', 'name', 'host', 'date', 'max_participant', 'deadline', 'region', 'photo', 'content', 'status', 'open_chat', 'comments', 'participant', 'waiter')
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:

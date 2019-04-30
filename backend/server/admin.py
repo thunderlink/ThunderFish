58b91ba8157d03083be95a7c5bb4466857f015a0
Sungchan Yi <calofmijuck@snu.ac.kr>
2019-04-30 11:32:55 +0900

    Create Models without some fields

--- backend/server/admin.py
+++ backend/server/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, Meeting, Comment
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Meeting)
+admin.site.register(Comment)
--- backend/server/models.py
+++ backend/server/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
+# Path to default image
+DEFAULT_IMAGE = '../../images/app_logo.png'
+pic_folder = './migrations/pic_folder'
+
 class User(models.Model):
     nickname = models.CharField(max_length=20)
-    # photo = models.ImageField(height_field=200, width_field=200)
+    photo = models.ImageField(upload_to=pic_folder, default=DEFAULT_IMAGE)
     email = models.EmailField(max_length=30)
     name = models.CharField(max_length=50)
     gender = models.IntegerField()
@@ -13,7 +17,7 @@ class User(models.Model):
     # comment
 
     def __str__(self):
-        return str(self.name)
+        return self.name
 
     class Meta:
         ordering = ('name', )
@@ -21,26 +25,33 @@ class User(models.Model):
 class Meeting(models.Model):
     name = models.CharField(max_length=50)
     host = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateTimeField()
+    date = models.DateTimeField('meeting date')
     # participant
     # waiter
     max_participant = models.IntegerField()
-    deadline = models.DateTimeField()
+    deadline = models.DateTimeField('meeting deadline')
     # region
-    # photo = models.ImageField(height_field=200, width_field=200)
+    photo = models.ImageField(upload_to=pic_folder, default=DEFAULT_IMAGE)
     content = models.CharField(max_length=500)
     # tag
-    status = models.IntegerField()
+    status = models.IntegerField() # 1 as pending, 0 as complete ?
     # comment
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         ordering = ('id', )
 
 class Comment(models.Model):
-    date = models.DateTimeField()
+    date = models.DateTimeField('comment date', auto_now_add=True)
+    comment_text = models.CharField(max_length=1000, default="Test Text")
     # parent_comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
     parent_meeting = models.ForeignKey(Meeting, on_delete=models.CASCADE)
     writer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.comment_text
 
     class Meta:
         ordering = ('id', )

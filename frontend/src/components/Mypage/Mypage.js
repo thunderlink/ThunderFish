c135aec2a0a0568a98e00c0a61c9851cf0c3c55f
KRKimDongWoo <dhsodhao52@naver.com>
2019-05-01 18:12:55 +0900

    Working on Mypage

---
+++ frontend/src/components/Mypage/Mypage.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import { Route, Switch, BrowserRouter } from 'react-router-dom'
+
+import Userdetail from './Userdetail'
+import Useredit from './Useredit'
+import Meetinglist from './Meetinglist'
+
+import report from '../../icons/report-button.png'
+import edit from '../../icons/edit-button.png'
+
+import "./Mypage.css"
+class Mypage extends Component {
+	render() {
+		const username = "Anonymous"
+		const detail = "Hello, world!"
+		return (
+			<div className="userpage">
+				<div class="head">
+					<div class="profile">
+						<div class="photo_cutter">
+							<img />
+						</div>
+					</div>
+					<div class="content">
+						<div class="userdtail">
+							<p class="username"> {username} </p>
+							<p class="description"> {detail} </p>
+						</div>
+					</div>
+					<div class="badge">
+						<div class="report_button">
+							<img 
+								src={report}
+							/>
+							<p> Report </p>
+						</div>
+						<div class="edit_button">
+							<img
+								src={edit}
+							/>
+							<p> Edit </p>
+						</div>
+					</div>			
+				</div>
+				<div class="usertab">
+					<ul class="list">
+						<li> Detail </li>
+						<li> Meetings </li>
+					</ul>
+					<div class="spacer" />
+				</div>
+				<div class="content">
+					<Userdetail />
+					<Meetinglist />
+				</div>
+			</div>
+		)
+	}
+
+}
+
+export default Mypage

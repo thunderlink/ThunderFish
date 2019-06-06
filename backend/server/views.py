df039d6ee5e84ec7aa9a43a4b16dba87e4699497
Sungchan Yi <calofmijuck@snu.ac.kr>
2019-06-06 12:21:21 +0900

    Comment Updates authentication

--- backend/server/views.py
+++ backend/server/views.py
@@ -153,12 +153,18 @@ class MeetingDetail(generics.RetrieveUpdateDestroyAPIView):
         return self.update(request, *args, **kwargs)
 
 class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwner, )
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
     def put(self, request, *args, **kwargs):
         comment = Comment.objects.get(pk=kwargs['pk'])
+        ## Check if the request was sent by the writer
+        token = request.headers['Authorization'].split()[1]
+        id = Token.objects.get(pk=token).user.profile
+        if comment.writer != id:
+            return Response({"You don't have permissions to perform this action."}, status=HTTP_403_FORBIDDEN)
+
         request.data['parent_meeting'] = comment.parent_meeting.id
         request.data['writer'] = comment.writer.id
         return self.update(request, *args, **kwargs)

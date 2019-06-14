31b874ad3ff4dd720d9a836211a3d957cb0b08c9
Sungchan Yi <calofmijuck@snu.ac.kr>
2019-06-14 10:48:37 +0900

    Back (#48)
    * from my brain
    
    * Try
    
    * Updated requirements
    
    * remove import
    
    * Update
    
    * Update travis
    
    * Temporary Test
    
    * view
    
    * Testing
    
    * SHA256
    
    * kako login
    
    * strange change on model
    
    * Pariticipant user's profile id and name
    
    * Join, accept meetings
    
    * Accept/Reject meetings
    
    * Split Views
    
    * Split Views
    
    * Delete views

---
+++ backend/server/views/__init__.py
@@ -0,0 +1,6 @@
+from .auth import *
+from .comment import *
+from .meeting import *
+from .notification import *
+from .profile import *
+from .search import *
---
+++ backend/server/views/search.py
@@ -0,0 +1,32 @@
+from ..models import Meeting
+from ..serializers import MeetingSerializer
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+
+
+class SearchResult(generics.ListCreateAPIView):
+    permission_classes = (AllowAny, )
+    queryset = None
+    serializer_class = MeetingSerializer
+
+    def get(self, request, *args, **kwargs):
+        name_result = Meeting.objects.filter(name__contains=kwargs['keyword'])
+        print(kwargs['keyword'])
+        tag_result = Meeting.objects.filter(tag_set__name__contains=kwargs['keyword'])
+        result = name_result | tag_result
+        self.queryset = result.distinct()
+        print(self.queryset)
+        return self.list(request, *args, **kwargs)
+
+
+class SearchLocation(generics.ListCreateAPIView):
+    permission_classes = (AllowAny, )
+    queryset = None
+    serializer_class = MeetingSerializer
+
+    # Latitude and longitude must be included in the request
+    def get(self, request, *args, **kwargs):
+        data = request.data
+        lat, long, dist = float(data['latitude']), float(data['longitude']), int(kwargs['dist'])
+        self.queryset = Meeting.distance_search(dist, lat, long)
+        return self.list(request, *args, **kwargs)

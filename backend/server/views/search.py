a954e354c5e431dbd82fecc4c8875e5db1416631
Sungchan Yi <calofmijuck@snu.ac.kr>
2019-06-17 11:08:07 +0900

    Sort result by increasing distance

--- backend/server/views/search.py
+++ backend/server/views/search.py
@@ -1,7 +1,9 @@
 from ..models import Meeting
 from ..serializers import MeetingSerializer
 from rest_framework import generics
+from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from rest_framework.status import HTTP_200_OK
 
 
 class SearchResult(generics.ListCreateAPIView):
@@ -14,18 +16,26 @@ class SearchResult(generics.ListCreateAPIView):
         dist_search_flag = data['dist_flag']
         title_search_flag = data['title_flag']
         tag_search_flag = data['tag_flag']
+        result_serialized = {}
 
         result = Meeting.objects.filter(status=0)
         if title_search_flag == "true":
             result = result.filter(name__contains=data['keyword'])
 
         if tag_search_flag == "true":
-            result = result.filter(tag_set__name__contains=data['tagword'])
-            ## Multiple Tag should be implemented
+            tag_list = data['tagword'].split()
+            for tag in tag_list:
+                result = result.filter(tag_set__name__contains=tag)
 
         if dist_search_flag == "true":
             lat, long, dist = float(data['latitude']), float(data['longitude']), int(data['dist'])
-            result = Meeting.distance_search(result, dist, lat, long)
+            ret = Meeting.distance_search(result, dist, lat, long)
+            result_serialized['length'] = len(ret)
+            for idx, item in enumerate(ret):
+                result_serialized[idx] = MeetingSerializer(item[0]).data
+        else:
+            result_serialized['length'] = len(queryset)
+            for idx, item in enumerate(queryset):
+                result_serialized[idx] = MeetingSerializer(item).data
 
-        self.queryset = result
-        return self.list(request, *args, **kwargs)
+        return Response(result_serialized, status=HTTP_200_OK)
